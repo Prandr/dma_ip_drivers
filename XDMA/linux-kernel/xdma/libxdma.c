4f2c8430aff553d5990c876e3d3469a2119e9e63
Andrey Luzhanskij <andrey.luzhanskij@helmholtz-berlin.de>
2026-02-24 12:23:01 +0100

    Attempt to fix polling failure on ARM
    Add explicit memory barrier.

--- XDMA/linux-kernel/xdma/libxdma.c
+++ XDMA/linux-kernel/xdma/libxdma.c
@@ -1936,10 +1936,12 @@ static long xdma_wait_for_transfer(struct xdma_engine *engine)
 #ifdef XDMA_POLL_MODE
 	unsigned long jiffies_limit= jiffies + timeout_jiffies;
 	unsigned int descriptors_count= get_initial_adj_count(engine, 0)+1;/*this gives the number of descriptors in an adjacent block*/
+	u32 poll_wb=0;
 	/*replicates behaviour of wait_for_completion for unified handling of transfer result*/
 	do
 	{	
-		u32 poll_wb=engine->poll_mode_wb.virtual_addr->completed_desc_count;
+		mb();/*some architectures may require explicit barrier for the writeback become visible*/
+		poll_wb=engine->poll_mode_wb.virtual_addr->completed_desc_count;
 		u32 current_completed_descriptors=poll_wb & WB_COUNT_MASK;
 		/*return a positive value. xdma_finalise_transfer will deal with it appropriately*/
 		if((current_completed_descriptors >= descriptors_count) || (poll_wb & WB_ERR_MASK))
